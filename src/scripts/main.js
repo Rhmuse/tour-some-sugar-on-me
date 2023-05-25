22c8335e1b2ad596080121d804c14857e6033bd7
Rhett Muse <rhett.muse123@gmail.com>
2023-05-25 11:37:01 -0500

    invoke bookings in main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,5 @@
 import { Bands } from "./Bands.js";
+import { Bookings } from './Bookings.js';
 
 let body = document.querySelector("body");
 
@@ -7,4 +8,4 @@ body.innerHTML = `<section id="bookings">Bookings</section>
 <section id="bands"></section>`
 
 Bands();
-
+Bookings(); 
